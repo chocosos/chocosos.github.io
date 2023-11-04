d35e1ad1cc1c3691c3f86f548ec08e5ae4663184
chocosos <149858666+chocosos@users.noreply.github.com>
2023-11-04 15:37:30 -0600

    Update cookiebot.js

--- cookiebot.js
+++ cookiebot.js
@@ -1,18 +1,8 @@
 var bob=20
-Game.registerMod('test mod',{
-  /*
-    what this example mod does:
-    -double your CpS
-    -display a little popup for half a second whenever you click the big cookie
-    -add a little intro text above your bakery name, and generate that intro text at random if you don't already have one
-    -save and load your intro text
-  */
+Game.registerMod('cookiebot',{
+
   init:function(){
-    Game.registerHook('reincarnate',function(){Game.mods['test mod'].addIntro();});
-    Game.registerHook('check',function(){if (!Game.playerIntro){Game.mods['test mod'].addIntro();}});
-    Game.registerHook('click',function(){Game.Notify(choose(['A good click.','A solid click.','A mediocre click.','An excellent click!']),'',0,0.5);});
-    Game.registerHook('cps',function(cps){return cps*2;});
-    Game.registerHook('draw',function(){Game.mods['test mod'].gogo();});
+    Game.registerHook('draw',function(){Game.mods['cookiebot'].gogo();});
   },
   save:function(){
     //note: we use stringified JSON for ease and clarity but you could store any type of string
@@ -20,16 +10,10 @@ Game.registerMod('test mod',{
   },
   load:function(str){
     var data=JSON.parse(str);
-    if (data.text) Game.mods['test mod'].addIntro(data.text);
+    if (data.text) Game.mods['cookiebot'].addIntro(data.text);
   },
   gogo:function(){
     Game.ClickCookie()
   },
-  addIntro:function(text){
-    //note: this is not a mod hook, just a function that's part of the mod
-    Game.playerIntro=text||choose(['oh snap, it\'s','watch out, it\'s','oh no! here comes','hide your cookies, for here comes','behold! it\'s']);
-    if (!l('bakerySubtitle')) l('bakeryName').insertAdjacentHTML('afterend','<div id="bakerySubtitle" class="title" style="text-align:center;position:absolute;left:0px;right:0px;bottom:32px;font-size:12px;pointer-events:none;text-shadow:0px 1px 1px #000,0px 0px 4px #f00;opacity:0.8;"></div>');
-    l('bakerySubtitle').textContent='~'+Game.playerIntro+'~';
-    bob++;
-  },
+
 });
