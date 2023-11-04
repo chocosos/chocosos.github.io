863c0d104d42b8167e012f514801965539390fc4
chocosos <149858666+chocosos@users.noreply.github.com>
2023-11-04 12:51:26 -0600

    Update cookiebot.js

--- cookiebot.js
+++ cookiebot.js
@@ -1,1 +1,12 @@
-var bob=199
+
+
+Game: {
+  UpdateMenu: () =>{
+    fragment.appendChild(Menu.heading('Activate CookieBot'));
+    fragment.appendChild(Menu.toggleButton('awaken','Activate Bob','Awakens Bob, the primordial cookie force.'));
+  }
+}
+
+'awaken': {
+  var bob=199
+}
