e3693e9d103057a317b9ec74adf0224e4ffb2d9d
chocosos <149858666+chocosos@users.noreply.github.com>
2023-11-05 16:29:32 -0700

    Update cookiebot.js

--- cookiebot.js
+++ cookiebot.js
@@ -1,29 +1,32 @@
-var bob=20;
-var cursor=0;
-var grandma=0;
-var farm=0;
-var mine=0;
-var factory=0;
-var bank=0;
-var temple=0;
-var wizardTower=0;
-var shipment=0;
-var alchemyLab=0;
-var portal=0;
-var timeMachine=0;
-var antimatterCondenser=0;
-var prism=0;
-var chancemaker=0;
-var fractalEngine=0;
-var javascriptConsole=0;
-var idleverse=0;
-var cortexBaker=0;
-var you=0;
-var next=" "
+
+    var bob=20;
+    var cursor=0;
+    var grandma=0;
+    var farm=0;
+    var mine=0;
+    var factory=0;
+    var bank=0;
+    var temple=0;
+    var wizardTower=0;
+    var shipment=0;
+    var alchemyLab=0;
+    var portal=0;
+    var timeMachine=0;
+    var antimatterCondenser=0;
+    var prism=0;
+    var chancemaker=0;
+    var fractalEngine=0;
+    var javascriptConsole=0;
+    var idleverse=0;
+    var cortexBaker=0;
+    var you=0;
+    var next=" "
+
 Game.registerMod('cookiebot',{
 
   init:function(){
     Game.registerHook('draw',function(){Game.mods['cookiebot'].gogo();});
+
   },
   save:function(){
     //note: we use stringified JSON for ease and clarity but you could store any type of string
@@ -33,16 +36,20 @@ Game.registerMod('cookiebot',{
     var data=JSON.parse(str);
     if (data.text) Game.mods['cookiebot'].addIntro(data.text);
   },
+
+
   gogo:function(){
+    let bob=0
+    let cursor = Game.Objects['Cursor'].price/Game.Objects['Cursor'].storedCps
     Game.ClickCookie()
-    cursor = Game.Objects['Cursor'].price/Game.Objects['Cursor'].cps
-    grandma = Game.Objects['Grandma'].price/Game.Objects['Grandma'].cps
-    farm = Game.Objects['Farm'].price/Game.Objects['Farm'].cps
-    mine = Game.Objects['Mine'].price/Game.Objects['Mine'].cps
-    factory = Game.Objects['Factory'].price/Game.Objects['Factory'].cps
-    bank = Game.Objects['Bank'].price/Game.Objects['Bank'].cps
-    
-    
+
+    let grandma = Game.Objects['Grandma'].price/Game.Objects['Grandma'].storedCps
+    let farm = Game.Objects['Farm'].price/Game.Objects['Farm'].storedCps
+    let mine = Game.Objects['Mine'].price/Game.Objects['Mine'].storedCps
+    let factory = Game.Objects['Factory'].price/Game.Objects['Factory'].storedCps
+    let bank = Game.Objects['Bank'].price/Game.Objects['Bank'].storedCps
+
+
   },
 
 });
